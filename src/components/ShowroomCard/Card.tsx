d485b44b1510b40158f8501f19d3f38936b8febb
João Pedro Caetano <106156212+caetanojpo@users.noreply.github.com>
2024-05-09 11:15:25 -0300

    cards updated

--- src/components/ShowroomCard/Card.tsx
+++ src/components/ShowroomCard/Card.tsx
@@ -11,61 +11,91 @@ const CardVehicle = (props: propsCardVehicle) => {
   return (
     <Flex
       bg="white"
-      p={4}
+      p={{ base: 4, md: 4 }}
       w="100%"
-      minW={'260px'}
-      maxW={'300px'}
+      minW={"260px"}
+      maxW={{ base: "600px", lg: "300px" }}
       h="100%"
-      minH='400px'
-      borderRadius="20px"
+      minH={{ base: "100px", md: "450px" }}
+      borderRadius={{ base: "10px", md: "20px" }}
       boxShadow="md"
       textAlign="center"
-      flexDir="column"
+      flexDir={{ base: "row", md: "column" }}
       justify="space-between"
-      alignItems="center"
-
-    ><Flex h='200px' w='100%' position={'relative'} mb="15px" justify='center'>
-
-      <Image
-        src={props.image}
-        alt={props.title}
-       style={{objectFit:'cover', objectPosition:'center', position:'absolute' }}
-        borderRadius="10px"
-        border="2px solid #cb3438"
-      />
-      </Flex>
-      <Text
-        color="primary"
-        fontFamily="JoyrideExt"
-        fontWeight="bold"
-        fontSize="16px"
-      >
-        {props.title}
-      </Text>
-      <Text
-        fontSize="14px"
-        fontFamily="Eina"
-        color="black"
-        width="240px"
-        fontWeight={700}
-        mb={4}
-      >
-        {props.description}
-      </Text>
-      <Text fontSize="xl" mb={4} fontFamily="Joyride" color="#919497">
-        r$ {props.price}
-      </Text>
+      alignItems={"center"}
+    >
       <Flex
-        as="a"
-        className="button"
-        bgColor="#CB3438"
-        href={props.link}
-        target="_blank"
+        h={{ base: "100%", md: "50%" }}
+        w={{ base: "50%", md: "80%", xl:'100%' }}
+        maxW={{base:'190px', lg:'300px'}}
+        position={"relative"}
+        mb={{ base: "0px", md: "15px" }}
+        justify="center"
+        align={"center"}
+        mr={{ base: "10px", md: "0px" }}
       >
-        <Text fontFamily="JoyrideExt" fontSize="15px" color="light">
-          {" "}
-          ver detalhes
+        <Image
+          src={props.image}
+          alt={props.title}
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+            position: "absolute",
+          }}
+          borderRadius="10px"
+          border="2px solid #cb3438"
+        />
+      </Flex>
+      <Flex
+        flexDir={"column"}
+        w={{ base: "50%", md: "100%" }}
+        align="center"
+        justify={{ base: "center", md: "space-between" }}
+        h={{ base: "100%", md: "50%" }}
+      >
+        <Text
+          color="primary"
+          fontFamily="JoyrideExt"
+          fontWeight="bold"
+          fontSize={{ base: "10px", md: "16px" }}
+        >
+          {props.title}
         </Text>
+        <Text
+          fontSize={{ base: "10px", md: "14px" }}
+          fontFamily="Eina"
+          color="black"
+          width={{ base: "80%", md: "240px" }}
+          fontWeight={700}
+          mb={4}
+        >
+          {props.description}
+        </Text>
+        <Text
+          fontSize={{ base: "12px", md: "xl" }}
+          mb={4}
+          fontFamily="Joyride"
+          color="#919497"
+        >
+          r$ {props.price}
+        </Text>
+        <Flex
+          as="a"
+          className="button"
+          bgColor="#CB3438"
+          href={props.link}
+          target="_blank"
+          padding={{ base: 4, md: 2 }}
+        >
+          <Text
+            fontFamily="JoyrideExt"
+            fontSize={{ base: "10px", md: "15px" }}
+            color="light"
+          >
+            {" "}
+            ver detalhes
+          </Text>
+        </Flex>
       </Flex>
     </Flex>
   );
